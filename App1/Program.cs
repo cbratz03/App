6515ef5fc27fb04d6388cb0aac0747742c032f58
cbratz03 <cbratz03@gmail.com>
2016-10-29 12:49:12 -0500

    Initial Commit

--- App1/Program.cs
+++ App1/Program.cs
@@ -10,6 +10,35 @@ namespace App1
     {
         static void Main(string[] args)
         {
+
+            //Adds two numbers together
+
+            int numOne;
+            int numTwo;
+            string expr;
+            int sum;
+
+            Console.WriteLine("Enter First Number:");
+            numOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Add Second Number:");
+            numTwo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter +, -, *, /");
+
+            expr = Console.ReadLine();
+
+            if (expr == "+")
+                sum = (numOne + numTwo);
+            else if (expr == "*")
+                sum = (numOne * numTwo);
+
+
+            Console.WriteLine(sum);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
         }
     }
 }
